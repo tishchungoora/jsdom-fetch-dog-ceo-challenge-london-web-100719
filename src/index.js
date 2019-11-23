38278ee8e6b01f60d72ba3b15dedb0b33bf11e4b
Tish Chungoora <tishal82@gmail.com>
2019-11-23 22:12:00 +0000

    Implemented challenge 4 filtering feature. Tested with other filtering letter of the alphabet.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ document.addEventListener("DOMContentLoaded", function() {
     fetchImageData();
     fetchBreedData();
 
+    // execute list filtering
+    filterList();
+
     // CHALLENGE 1
     
     // fetch json for images and render images
@@ -49,16 +52,38 @@ document.addEventListener("DOMContentLoaded", function() {
             let dogLiEl = document.createElement("li");
             dogLiEl.innerText = key;
             breedsList.appendChild(dogLiEl);
+
             dogLiEl.style.cursor = "pointer";
-            dogLiEl.addEventListener("click", changeColor); 
+            dogLiEl.addEventListener("click", changeColor);
+
+            dogLiEl.setAttribute("class", `${key.charAt(0)}`);
         }
     }
     
     // CHALLENGE 3
-    
+
     // using event.target to implement event delegation
     function changeColor(event) {
       event.target.style.color = '#d00';
+    }
+
+    // CHALLENGE 4
+    
+    function filterList() {
+        const dropDown = document.querySelector("#breed-dropdown");
+
+        dropDown.addEventListener("change", function() {
+            liArr = document.querySelectorAll("li");
+            ddValue = dropDown.value;
+
+            liArr.forEach(function(li) {
+                li.style.display = "";
+
+                if (li.className !== ddValue && ddValue !== "All") {
+                    li.style.display = "none";
+                }
+            })
+        })
     }
 
 })
